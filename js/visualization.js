ebe2c4d761f7e918cb45ce989c86101daf244b53
raomegha <rao.m@husky.neu.edu>
2020-04-06 00:40:33 -0400

    test

--- js/visualization.js
+++ js/visualization.js
@@ -2,6 +2,16 @@
 // variables and prevent 
 ((() => {
 
-  console.log("Hello, world!");
+  // Load the data from a json file (you can make these using
+  // JSON.stringify(YOUR_OBJECT), just remove the surrounding "")
+  d3.json("data/memberList.json", (data) => {
 
+    // Create a table given the following: 
+    // a dispatcher (d3-dispatch) for selection events; 
+    // a div id selector to put our table in; and the data to use.
+    let tableData = table()
+      .selectionDispatcher(d3.dispatch(dispatchString))
+      ("#table", data);
+  
+    });
 })());
