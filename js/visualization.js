3625ad6ef9bbac306a728fcf62de83c76a9324cf
raomegha <rao.m@husky.neu.edu>
2020-04-09 13:27:23 -0400

    updates

--- js/visualization.js
+++ js/visualization.js
@@ -6,6 +6,10 @@
   // JSON.stringify(YOUR_OBJECT), just remove the surrounding "")
   d3.json("data/memberList.json", (data) => {
 
+    // General event type for selections, used by d3-dispatch
+    // https://github.com/d3/d3-dispatch
+    const dispatchString = "selectionUpdated";
+
     // Create a table given the following: 
     // a dispatcher (d3-dispatch) for selection events; 
     // a div id selector to put our table in; and the data to use.
